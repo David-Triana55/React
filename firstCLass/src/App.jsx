9c54dc73d913b9404a291b50e3a9fa7956473294
David Triana <davidtriana796@gmail.com>
2024-03-20 11:21:37 -0500

    vite

---
+++ firstCLass/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import './TwitterFollowCard'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+function App(){
+    return (
+        <div className='App'>
+            <TwitterFollowCard
+                initialFollowing
+                userName='midudev'
+                name='miguel angel duran' 
+            />
+            <TwitterFollowCard 
+                initialFollowing    
+                userName='pheralb'
+                name='pablo hernandez' 
+            />
+            <TwitterFollowCard 
+
+                initialFollowing
+                userName='elonmusk'
+                name='Elon Musk'
+            />
+            <TwitterFollowCard 
+                
+                initialFollowing
+                userName='vxnder'
+                name='Vanderhart' 
+            />
+
+        </div>
+    )
+
+}
+
+export {App}
