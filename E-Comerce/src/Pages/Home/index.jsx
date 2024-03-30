6c37b97b1f1e9e1f4ee0cb4f2b76741eda99c793
David Triana <davidtriana796@gmail.com>
2024-03-30 12:39:22 -0500

    card e-comerce api

--- E-Comerce/src/Pages/Home/index.jsx
+++ E-Comerce/src/Pages/Home/index.jsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from "react"
+import Card from "../../Components/Card"
 import Layout from "../../Components/Layout"
 
 
 function Home(){
+    const [items , setItems] = useState(null)
+
+    useEffect(()=> {
+        const fetchData =  async () => {
+            try{
+                const res = await fetch('https://api.escuelajs.co/api/v1/products')
+                const data =  await res.json()
+                console.log(data);
+                setItems(data)
+            } catch(err) {
+                console.error(err);
+            }
+        }
+        fetchData()
+    }, [])
     return (
         <Layout>
-            home
+            <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+                {
+                    items?.map(item=> (
+                        <Card
+                            key={item.id}
+                            data={item}
+                        />
+                    ))
+                }
+            </div>
         </Layout>
     )
 }
